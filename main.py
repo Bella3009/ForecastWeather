9ad84b379dd5d0ba378742b35091ac3c15700c95
Bella3009 <bellasara13@gmail.com>
2023-05-30 21:48:46 +0200

    Add a line graph with fake data

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days max 5 days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+temp_dates = ["2022-10-25", "2022-10-26", "2022-10-27"]
+temp_temp = [10, 11, 15]
+
+figure = px.line(x=temp_dates, y=temp_temp, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
