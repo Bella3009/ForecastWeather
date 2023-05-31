6de35cdad3f3b422ac0c051f65528f5fe6d8ab39
Bella3009 <bellasara13@gmail.com>
2023-05-31 21:56:43 +0200

    Getting all the data according to the place and the number of days

--- backend.py
+++ backend.py
@@ -1,11 +1,19 @@
 import requests
 API_Key = "3cb4d4b408331132fc3ca01b1f17ac69"
 
-def get_data(place, fore_days=None, kind=None):
+
+def get_data(place, fore_days, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_Key}"
     response = requests.get(url)
     content = response.json()
-    return content
+
+    # Filter the data
+    data = content["list"]
+    # The data is taken every 3 hours therefore 3/24 = 8
+    points = 8 * fore_days
+    filter_data = data[:points]
+    return filter_data
+
 
 if __name__ == "__main__":
-    print(get_data(place="Ragusa"))
+    print(get_data(place="Ragusa", fore_days=2))
