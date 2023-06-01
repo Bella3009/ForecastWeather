47e60572f746c3fd7b916999c0f56a11504cab0c
Bella3009 <bellasara13@gmail.com>
2023-06-01 21:48:51 +0200

    Final touches to remove small errors like temperature which needed to be divided by 10 and debug when the user write a place wrongly.

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-if place:
-    data = gt(place, days)
-    dates = [dict["dt_txt"] for dict in data]
+try:
+    if place:
+        data = gt(place, days)
+        dates = [dict["dt_txt"] for dict in data]
 
-    if option == "Temperature":
-        temp = [dict["main"]["temp"] for dict in data]
-        # Create graph
-        figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky = [dict["weather"][0]["main"] for dict in data]
-        image_path = [images[condition] for condition in sky]
+        if option == "Temperature":
+            temp = [dict["main"]["temp"] / 10 for dict in data]
+            # Create graph
+            figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky = [dict["weather"][0]["main"] for dict in data]
+            image_path = [images[condition] for condition in sky]
 
-        st.image(image_path, width=115)
+            st.image(image_path, width=115)
+except KeyError:
+    st.warning("The place chosen is written incorrectly.")
